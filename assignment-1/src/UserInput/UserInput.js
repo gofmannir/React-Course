be380495184e0df628d4ac66fa9b772d4130c689
Nir Gofman <nirgofman@Nirs-MacBook-Pro.local>
2020-07-28 15:57:29 +0300

    First Upload

--- assignment-1/src/UserInput/UserInput.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UserInput = (props) => {
-    return (
-        <input value={props.currentUsername} onChange={props.event_handler} type='text' />
-    )
-}
-
-export default UserInput
